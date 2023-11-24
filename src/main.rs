fa2809bcc5425986decf14f9c6c04b632993a659
AB7090 <AB7090@student.jamk.fi>
2023-11-24 04:50:58 +0200

    update routing and tests

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,41 @@
-use axum::response::Html;
-use axum::routing::get;
+use axum::extract::Query;
+use axum::http::StatusCode;
+use axum::response::{Html, IntoResponse};
+use axum::routing::{get, Route};
 use axum::Router;
-use std::error::Error;
+use serde::Deserialize;
 use std::net::SocketAddr;
 
+mod web;
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() {
+    // ---- POSTGRES ----
     let connectionstring = "postgresql://postgres:password@localhost/test";
-    let connpool = sqlx::postgres::PgPool::connect(connectionstring).await?;
+    let connpool = sqlx::postgres::PgPool::connect(connectionstring)
+        .await
+        .unwrap();
+    sqlx::migrate!("./migrations").run(&connpool).await.unwrap();
 
-    sqlx::migrate!("./migrations").run(&connpool).await?;
+    // ---- ROUTES ----
+    let routes = Router::new().merge(routes());
 
-    dbg!(connpool.options());
-
-    let routes = Router::new().route("/", get(|| async { Html("hello") }));
-
+    // ---- SERVER ----
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("listening on {addr}");
 
     axum::Server::bind(&addr)
         .serve(routes.into_make_service())
-        .await?;
+        .await
+        .unwrap();
+}
 
-    Ok(())
+fn routes() -> Router {
+    Router::new().route("/", get(handler))
+}
+
+async fn handler() -> impl IntoResponse {
+    println!("{:12} - handler", "HANDLER");
+
+    Html("hello")
 }
--- tests/response.rs
+++ tests/response.rs
@@ -1,8 +1,26 @@
-use anyhow::Result;
+use reqwest::{self, Client};
+use std::error::Error;
 
 #[tokio::test]
-async fn response() -> Result<()> {
-    reqwest::get("http://127.0.0.1:8080").await?.print().await?;
+async fn response() -> Result<(), Box<dyn Error>> {
+    let client = Client::new();
 
+    let binding = client
+        .get("http://localhost:8080/")
+        .send()
+        .await
+        .expect("failed to get response");
+
+    let res = client
+        .get("http://localhost:8080/")
+        .send()
+        .await
+        .expect("failed to get response")
+        .text()
+        .await
+        .expect("Failed to get payload");
+
+    println!("{:?}", binding.status());
+    println!("{}", res);
     Ok(())
 }
