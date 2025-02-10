5faa99a3f8eece9efadd8dba5072609a426598d0
SteamFindu <arttu.hiekkanen@hotmail.com>
2025-02-10 15:00:39 +0200

    clean up main

--- src/main.rs
+++ src/main.rs
@@ -78,18 +78,6 @@ async fn main() {
 
     let app = create_router(Arc::new(app_state.clone()));
 
-    tokio::spawn(redirect_http_to_https(config.clone()));
-
-    /*
-    if cfg!(debug_assertions) {
-        let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-        println!("listening on {addr}");
-        axum_server::bind(addr)
-            .serve(app.into_make_service())
-            .await
-            .unwrap();
-    } else {
-    */
     let keyconfig = RustlsConfig::from_pem_file(
         PathBuf::from("/etc/letsencrypt/live/findu.dev/cert.pem"),
         PathBuf::from("/etc/letsencrypt/live/findu.dev/privkey.pem"),
@@ -104,57 +92,4 @@ async fn main() {
         .await
         .unwrap();
     //};
-
-    /*
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", &config.port))
-        .await
-        .unwrap();
-
-    axum::serve(listener, app).await.unwrap();
-    */
-}
-
-#[allow(dead_code)]
-async fn redirect_http_to_https(config: Config) {
-    fn make_https(host: &str, uri: Uri, https_port: u16) -> Result<Uri, BoxError> {
-        let mut parts = uri.into_parts();
-
-        parts.scheme = Some(axum::http::uri::Scheme::HTTPS);
-
-        if parts.path_and_query.is_none() {
-            parts.path_and_query = Some("/".parse().unwrap());
-        }
-
-        let authority: Authority = host.parse()?;
-        let bare_host = match authority.port() {
-            Some(port_struct) => authority
-                .as_str()
-                .strip_suffix(port_struct.as_str())
-                .unwrap()
-                .strip_suffix(':')
-                .unwrap(), // if authority.port() is Some(port) then we can be sure authority ends with :{port}
-            None => authority.as_str(),
-        };
-
-        parts.authority = Some(format!("{bare_host}:{https_port}").parse()?);
-
-        Ok(Uri::from_parts(parts)?)
-    }
-
-    let redirect = move |Host(host): Host, uri: Uri| async move {
-        match make_https(&host, uri, config.https) {
-            Ok(uri) => Ok(Redirect::permanent(&uri.to_string())),
-            Err(error) => {
-                println!("failed to convert URI to HTTPS with err {}", error);
-                Err(StatusCode::BAD_REQUEST)
-            }
-        }
-    };
-
-    let addr = SocketAddr::from(([0, 0, 0, 0], config.http));
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    println!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, redirect.into_make_service())
-        .await
-        .unwrap();
 }
