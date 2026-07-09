b1346afaf49e6d535837c08ecc437ec6cae82fc1
SteamFindu <arttu.hiekkanen@hotmail.com>
2026-07-09 19:08:09 +0300

    fix most basic issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react'
 import { lingui } from '@lingui/vite-plugin'
 
 export default defineConfig({
-  plugins: [react(), lingui()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ["@lingui/babel-plugin-lingui-macro"],
+      },
+    }),
+    lingui(),
+  ],
   server: {
     proxy: {
       '/api': {
